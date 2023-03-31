ae2d5aaeec5f6658224c90451ab66ca3f7fbe64a
jungheeeun <jungheeeun272@gmail.com>
2023-03-31 10:35:24 +0900

    자바스크립트- 13  변수와 배열 다시 기본기, 그리고 활용

---
+++ js-13.js
@@ -0,0 +1,19 @@
+//! 변수와 배열 예제
+
+//변수 예제 
+let _kdt = 'kong' ;
+console.log(_kdt); //'kong'
+
+_kdt = 'ukjae';
+console.log(_kdt); //'ukjae'
+
+//배열 예제
+let numbers = [1,2,3,4,5];
+console.log(numbers);  // [1,2,3,4,5]
+
+numbers = ["피카츄", "라이츄", "파이리" ,"꼬부기", "버터풀"] ;
+console.log(numbers);// ["피카츄", "라이츄", "파이리" ,"꼬부기", "버터풀"]
+// kong
+// ukjae
+// [ 1, 2, 3, 4, 5 ]
+// [ '피카츄', '라이츄', '파이리', '꼬부기', '버터풀' ]
